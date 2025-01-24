d65ca2ea5673cac7b54883f5c7656d8e396610b0
xAdri <aperezgfx@gmail.com>
2025-01-24 20:35:48 +0100

    Add config

---
+++ Assets/Scripts/Animal.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour
+{
+    [SerializeField] private GameObject targetPrefab;
+    [SerializeField] private List<Transform> targetPositions;
+    [SerializeField] private Transform nextPos;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
