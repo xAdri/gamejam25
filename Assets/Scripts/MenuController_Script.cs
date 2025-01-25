bdfb91f4e2d7c310872653653eb37f5887cc11d1
Sadh0w <alexgalindosandin@gmail.com>
2025-01-25 15:43:44 +0100

    Menu pausa

--- Assets/Scripts/MenuController_Script.cs
+++ Assets/Scripts/MenuController_Script.cs
@@ -7,24 +7,38 @@ public class MenuControler_Script : MonoBehaviour
     public GameObject endPanel;
     public TextMeshProUGUI[] countText;
     public GameObject[] pauseUI;
+    public bool pause, unPause;
     public void TransitionScene(int level)
     {
         SceneManager.LoadScene(level);
     }
 
-    public void Pause() 
+    public void Pause()
     {
-        Time.timeScale = 0;
-        pauseUI[0].SetActive(false);
-        pauseUI[1].SetActive(true);
+        if (pauseUI.Length > 1)
+        {
+            Time.timeScale = 0;
+            pauseUI[0].SetActive(false);
+            pauseUI[1].SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("pauseUI array does not have the expected number of elements.");
+        }
     }
 
     public void UnPause()
     {
-        Time.timeScale = 1;
-        pauseUI[0].SetActive(true);
-        pauseUI[1].SetActive(false);
-
+        if (pauseUI.Length > 1)
+        {
+            Time.timeScale = 1;
+            pauseUI[0].SetActive(true);
+            pauseUI[1].SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("pauseUI array does not have the expected number of elements.");
+        }
     }
 }
 
